45c4ead9ad44e04910ee7b92ef764cd3dc5bacab
Young1 <dlduddnjs4798@gmail.com>
2023-05-03 06:27:58 +0900

    SecondCourse 구현

--- enol/src/main/java/com/graduatepj/enol/makeCourse/dto/SecondCourse.java
+++ enol/src/main/java/com/graduatepj/enol/makeCourse/dto/SecondCourse.java
@@ -1,4 +1,19 @@
 package com.graduatepj.enol.makeCourse.dto;
 
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SecondCourse {
+    private CourseDto selectedCourse;
+    private List<String> detailCategories;
+
+    public SecondCourse(CourseDto selectedCourse) {
+        this.selectedCourse = selectedCourse;
+    }
 }
